c3fdea26c33ba80dab159b8159aad86d08469308
Joe McClure <jdmcclur@us.ibm.com>
2016-08-25 11:39:20 -0500

    Initial java bookingservice commit

---
+++ src/main/java/com/acmeair/config/BookingLoaderREST.java
@@ -0,0 +1,28 @@
+package com.acmeair.config;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import com.acmeair.loader.BookingLoader;
+
+
+@Path("/bookings/loader")
+public class BookingLoaderREST {
+
+	//	private static Logger logger = Logger.getLogger(LoaderREST.class.getName());
+
+	@Inject
+	private BookingLoader loader;	
+	
+	@GET
+	@Path("/load")
+	@Produces("text/plain")
+	public Response loadDB() {	
+		String response = loader.clearBookingDB();
+		return Response.ok(response).build();	
+	}
+
+}
---
+++ src/main/java/com/acmeair/web/BookingServiceApp.java
@@ -0,0 +1,20 @@
+package com.acmeair.web;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import com.acmeair.config.BookingConfiguration;
+import com.acmeair.config.BookingLoaderREST;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/rest/api")
+
+public class BookingServiceApp extends Application {
+    public Set<Class<?>> getClasses() {
+        return new HashSet<Class<?>>(Arrays.asList(BookingServiceREST.class,BookingConfiguration.class,BookingLoaderREST.class));
+    }
+}
